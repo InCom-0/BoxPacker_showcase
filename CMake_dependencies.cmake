00fd3542cb4a2a9a618f5ecb5efd61b15a96b136
Michal Lurie <Michal.Lurie@gmail.com>
2026-05-16 12:19:32 +0200

    Feat: initial working setup

---
+++ CMake_dependencies.cmake
@@ -0,0 +1,4 @@
+include(cmake/CPM_0.42.1.cmake)
+
+
+CPMAddPackage("gh:InCom-0/incstd#main")
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include <cstdio>
+#include <print>
+#include <string_view>
+
+
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
