03a1d6701443074b9256b97935ce5dec8fc9b40c
Michal Lurie <Michal.Lurie@gmail.com>
2026-05-16 12:23:29 +0200

    Initial

--- CMake_dependencies.cmake
+++
@@ -1,4 +0,0 @@
-include(cmake/CPM_0.42.1.cmake)
-
-
-CPMAddPackage("gh:InCom-0/incstd#main")
--- src/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <cstdio>
-#include <print>
-#include <string_view>
-
-
-
-int main(int argc, char *argv[]) {
-    return 0;
-}
